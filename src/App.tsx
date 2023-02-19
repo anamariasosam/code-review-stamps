3f7647767ace4d9a96e67fcad52fa405bc0ff58a
Ana Sosa <anamariasosam@gmail.com>
2023-02-19 23:02:19 +0000

    feat(Prettier): Add dev dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
 function App() {
   const [count, setCount] = useState(11)
 
-  return (<p>{count}</p>)
+  return <p>{count}</p>
 }
 
 export default App
