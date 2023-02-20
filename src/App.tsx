8c5896c1dce70e21e03eec3ba9bba6b7ef51992c
Ana Sosa <anamariasosam@gmail.com>
2023-02-20 00:25:16 +0000

    feat(SASS): add sass dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import {useState} from 'react'
-
+import './App.scss'
 function App() {
-  const [count, setCount] = useState(11)
+  const [count, setCount] = useState(0)
 
-  return <p>{count}</p>
+  return (
+    <main className="App__section">
+      <button onClick={() => setCount(count - 1)} className="App__button">
+        -
+      </button>
+      <h1 className="App__title">{count}</h1>
+      <button onClick={() => setCount(count + 1)} className="App__button">
+        +
+      </button>
+    </main>
+  )
 }
 
 export default App
